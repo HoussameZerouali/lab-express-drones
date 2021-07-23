561d2e268aad574434bf496cdbc5789856efdc5d
Houssame Zerouali <azenax328@gmail.com>
2021-07-23 15:38:52 +0200

    finished the fourth iteration

--- routes/drones.js
+++ routes/drones.js
@@ -49,12 +49,24 @@ router.post('/drones/create', (req, res, next) => {
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  droneModel.findById(req.params.id)
+    .then((result) => {
+      res.render('drones/update-form', {
+        drone: result
+      })
+    })
+  
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
-  // ... your code here
+  return droneModel.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect('/drones')
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
